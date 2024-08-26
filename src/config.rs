09c637b59324f22d5dcbf8d9401fe6bd1ec19837
Aljoscha Pörtner <aljoscha.poertner@proton.me>
2024-08-26 20:50:08 +0200

    test(config): add simple test case

--- src/config.rs
+++ src/config.rs
@@ -14,3 +14,24 @@ impl RunoConfig {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::*;
+
+    use crate::k8s::K8s;
+
+    use super::RunoConfig;
+
+    #[fixture]
+    fn valid_k8s() -> K8s {
+        return K8s { dry_run: false };
+    }
+
+    #[rstest]
+    #[case(10)]
+    fn build_valid_requeue_duration(valid_k8s: K8s, #[case] requeue_duration: u64) {
+        let config = RunoConfig::build(valid_k8s, requeue_duration);
+        assert_eq!(config.requeue_duration, requeue_duration)
+    }
+}
