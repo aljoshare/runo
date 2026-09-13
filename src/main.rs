6d7336fc580c342112fa702364b213e191d07c5c
Aljoscha Bollmann <aljoscha.bollmann@proton.me>
2026-09-13 16:47:17 +0200

    feat(cli): add --one-shot flag and execution mode dispatch (T018)
    - add --one-shot boolean flag to MainArgs
    - validate requeue_duration is greater than 0
    - dispatch execution mode to run_one_shot without starting actix-web server
    
    Signed-off-by: Aljoscha Bollmann <aljoscha.bollmann@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ struct MainArgs {
     http_port: u16,
     #[clap(long, default_value_t = false)]
     dry_run: bool,
+    #[clap(long, default_value_t = false)]
+    one_shot: bool,
     #[clap(long, default_value_t = String::from("reconciliation"))]
     mode: String,
     #[clap(long, default_value_t = 300)]
@@ -33,6 +35,9 @@ struct MainArgs {
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = MainArgs::parse();
+    if args.requeue_duration == 0 {
+        return Err(anyhow!("requeue-duration must be greater than 0"));
+    }
     let subscriber = match logging::get_subscriber(true) {
         Ok(s) => {
             info!("Logging initialized..");
@@ -43,24 +48,23 @@ async fn main() -> anyhow::Result<()> {
     subscriber.init();
     let k8s = K8s::build(args.dry_run);
     let config = RunoConfig::build(k8s, args.requeue_duration);
-    match args.mode.as_str() {
-        "reconciliation" => {
-            info!("Running runo in reconciliation mode.");
-            let http_server_result = http::run_http_server(args.http_port);
-            let reconciler = reconciler::run_with_reconciliation(config);
-            match http_server_result {
-                Ok(http_server) => {
-                    tokio::join!(reconciler, http_server).1.unwrap();
-                    Ok(())
-                }
-                Err(_) => Err(anyhow!("Can't bind HTTP server to port!")),
+    let is_one_shot = args.one_shot || args.mode == "one-shot";
+    if is_one_shot {
+        info!("Running runo in one-shot mode.");
+        reconciler::run_one_shot(config).await?;
+        Ok(())
+    } else if args.mode == "reconciliation" {
+        info!("Running runo in reconciliation mode.");
+        let http_server_result = http::run_http_server(args.http_port);
+        let reconciler = reconciler::run_with_reconciliation(config);
+        match http_server_result {
+            Ok(http_server) => {
+                tokio::join!(reconciler, http_server).1.unwrap();
+                Ok(())
             }
+            Err(_) => Err(anyhow!("Can't bind HTTP server to port!")),
         }
-        "one-shot" => {
-            info!("Running runo in one-shot mode.");
-            reconciler::run_one_shot(config).await;
-            Ok(())
-        }
-        _ => Err(anyhow!("Mode is not supported!: {:?}", args.mode)),
+    } else {
+        Err(anyhow!("Mode is not supported!: {:?}", args.mode))
     }
 }
